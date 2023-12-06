00d01961ad07724e9c53efdb438aa456deda59a7
Facundo Noya <facunoya@hotmail.com>
2023-12-06 19:36:37 -0300

    vista inicial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Battleground from './components/Fights/Battleground';
+import Multiverse from './components/Multiverse/Multiverse';
+import { useState } from 'react';
 
 function App() {
+  const [userPage, setUserPage] = useState(1)
   return (
     <div className="App">
-      <Battleground></Battleground>
+      {userPage && userPage === 1 && <Multiverse setUserPage={setUserPage}></Multiverse>}
+      {userPage && userPage === 2 && <Battleground></Battleground>}
+
     </div>
   );
 }
---
+++ src/components/Multiverse/Multiverse.js
@@ -0,0 +1,13 @@
+import { useStatet } from "react";
+const Multiverse = ({ setUserPage }) => {
+
+    return (
+        <div>
+            <h1>Bienvenido</h1>
+            <input type="submit" onClick={(e) => { setUserPage(2); console.log(e.target.value) }} value={'Fight'} />
+            <input type="submit" onClick={(e) => { console.log(e.target.value) }} value={'Fighters'} />
+            <input type="submit" onClick={(e) => { console.log(e.target.value) }} value={'Bag'} />
+        </div>
+    )
+}
+export default Multiverse
