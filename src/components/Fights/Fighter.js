9b33c64c89caa66e00ada3b57666ad5e848f5dc7
diegonoya9 <diegonoya9@gmail.com>
2023-12-08 12:48:34 -0300

    downloaded new images

--- src/components/Fights/Fighter.js
+++ src/components/Fights/Fighter.js
@@ -4,7 +4,7 @@ import LifeBar from '../UI/LifeBar'
 const Fighter = ({ user, fighter }) => {
     return (
         <div className={user === "user" ? `${classes.userFighterWrapper} ${classes.fighterWrapper}` : `${classes.enemyFighterWrapper} ${classes.fighterWrapper}`}>
-            <h3 style={{ color: "white" }}>{fighter.name}</h3>
+            <h3 style={{ color: "white" }}>{fighter.name}/LVL:{fighter.level}</h3>
             <LifeBar currentHP={fighter.currentHP} maxHP={fighter.maxHP}></LifeBar>
             <div className={user === "user" ? classes.userFighter : classes.enemyFighter}>
                 <img alt="fighter" src={user === "user" ? `${fighter.imgBack}` : `${fighter.imgFront}`} className={classes.fighterImage} />
