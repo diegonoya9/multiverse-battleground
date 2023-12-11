e916e9fb327b25af253240002f7f8272f0b8b464
Facundo Noya <facunoya@hotmail.com>
2023-12-11 19:44:29 -0300

    style de bag

--- src/components/Multiverse/ObjectsPage.js
+++ src/components/Multiverse/ObjectsPage.js
@@ -7,15 +7,17 @@ const ObjectsPage = ({ user, changeMultiverseActivePage }) => {
         display: 'none',
     };
     return (<div>
-        <ReactAudioPlayer src={musicFile} autoPlay controls style={audioStyle} />
-        <button value="Back to Main Menu" onClick={() => { changeMultiverseActivePage("mainMenu") }} >Back to Main Menu </button>
+        <ReactAudioPlayer src={musicFile} /*autoPlay*/ controls style={audioStyle} />
+        <button value="Back to Main Menu" className={classes.backToMainMenuBtn} onClick={() => { changeMultiverseActivePage("mainMenu") }} >Back to Main Menu </button>
         <div className={classes.container} >
             {user &&
                 user.objects.map((object) => {
                     return (
                         <div className={classes.objectContainer} key={object.name}>
                             <span className={classes.objectName}>{object.name}</span>
-                            <img alt="fighter" src={object.img} className={classes.objectImg} />
+                            <div className={classes.imageContainer}>
+                                <img alt="fighter" src={object.img} className={classes.objectImg} />
+                            </div>
                             <span className={classes.objectName}>Quantity:{object.quantity}</span>
                         </div>
                     );
