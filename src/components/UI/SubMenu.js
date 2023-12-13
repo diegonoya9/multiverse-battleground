093793b5881bc0c270e4bf8cbcd21c65f7af7b15
diegonoya9 <diegonoya9@gmail.com>
2023-12-12 23:33:45 -0300

    fixed fighters selector to show only the ones in the activeParty

--- src/components/UI/SubMenu.js
+++ src/components/UI/SubMenu.js
@@ -33,7 +33,7 @@ const SubMenu = ({ user, clickHandler, toggleSubMenu, selectedOption, userFighte
                 return x.category === "battleItem" && <li key={x.name + i} className={classes.options} onClick={() => { toggleSubMenu(); clickHandler(x, selectedOption); }}><div>{x.name}:{x.quantity}<img alt="fighter mini" src={x.img} className={classes.miniImgMenu} /></div></li>
             }
             )}
-            {optionsArray && selectedOption === "fighters" && optionsArray.map((x, i) => { return <li key={x.name + i} className={classes.options} onClick={() => { toggleSubMenu(); clickHandler(x, selectedOption); changeFighter(x); }}>{x.name}<img alt="fighter mini" src={x.imgFront} className={classes.miniImgMenu} /></li> })}
+            {optionsArray && selectedOption === "fighters" && optionsArray.map((x, i) => { return x.inParty && <li key={x.name + i} className={classes.options} onClick={() => { toggleSubMenu(); clickHandler(x, selectedOption); changeFighter(x); }}>{x.name}<img alt="fighter mini" src={x.imgFront} className={classes.miniImgMenu} /></li> })}
 
             <div className={classes.attack}></div>
         </ul>
