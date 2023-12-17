d984e32df388056831ec8a6dc62b9b402bdeb55a
diegonoya9 <diegonoya9@gmail.com>
2023-12-17 12:53:52 -0300

    fixed css for fighters and lifebar

--- src/components/Fights/Fighter.js
+++ src/components/Fights/Fighter.js
@@ -4,15 +4,19 @@ import LifeBar from './LifeBar'
 
 const Fighter = ({ children, user, fighter, styleType }) => {
     return (
-        <div className={user === "user" ? `${classes.userFighterWrapper} ${classes.sombra} ${classes.fighterWrapper}` : `${classes.enemyFighterWrapper} ${classes.fighterWrapper}`}>
-            <h3 className={classes[styleType]} >{fighter.name}/LVL:{fighter.level}</h3>
-            <LifeBar styleType={styleType} currentHP={fighter.currentHP} maxHP={fighter.maxHP}></LifeBar>
-            <div className={user === "user" ? classes.userFighter : classes.enemyFighter}>
-                {children}
-                <img alt="fighter" src={user === "user" ? `${fighter.imgBack}` : `${fighter.imgFront}`} className={classes.fighterImage} />
-                <div className={classes.fighterShadow}>Shadow</div>
+        <div className={`${classes.fullContainer} ${user === "user" ? classes.userFighterWrapper : classes.enemyFighterWrapper}`}>
+            <div className={`${classes[styleType]} ${classes.headerContainer} ${user === "user" ? classes.userHeader : classes.enemyHeader}`}>
+                <h3 className={`${classes[styleType + 'h3']} ${classes.hp}`} >{fighter.name}/LVL:{fighter.level}</h3>
+                <LifeBar styleType={styleType} currentHP={fighter.currentHP} maxHP={fighter.maxHP}></LifeBar>
             </div>
-        </div >
+            <div className={`${classes.sombra} ${classes.fighterWrapper}`}>
+                <div className={user === "user" ? classes.userFighter : classes.enemyFighter}>
+                    {children}
+                    <img alt="fighter" src={user === "user" ? `${fighter.imgBack}` : `${fighter.imgFront}`} className={classes.fighterImage} />
+                    <div className={classes.fighterShadow}>Shadow</div>
+                </div>
+            </div >
+        </div>
     )
 }
 
