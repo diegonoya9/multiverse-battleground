d2ee086f96236f5213727d498e73890b202a6324
diegonoya9 <diegonoya9@gmail.com>
2023-12-26 18:34:08 -0300

    changed backendurl

--- src/context/MyContext.js
+++ src/context/MyContext.js
@@ -5,8 +5,7 @@ const MyContext = createContext();
 const MyContextProvider = ({ children }) => {
     const [userContext, setUserContext] = useState({
         idUsuario: 1,
-        backEndUrl: "http://localhost:3009/api"
-        // Otros datos relacionados con el usuario si es necesario
+        backEndUrl: "https://graceful-capris-deer.cyclic.app/api" // Otros datos relacionados con el usuario si es necesario
     });
 
     const setUserId = (newId) => {
