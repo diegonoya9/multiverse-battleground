62ab6e69dc9370b35144c652670f3d94af5128e6
Diego Noya <diegonoya9@gmail.com>
2024-01-15 09:43:20 -0300

    added google login

--- src/components/Multiverse/Home.js
+++ src/components/Multiverse/Home.js
@@ -1,12 +1,18 @@
-import { useState } from "react"
+import { useState,useContext } from "react"
 import Multiverse from "./Multiverse"
 import Modal from "../UI/Modal"
 import Battleground from "../Fights/Battleground"
 import classes from './Home.module.css'
 import Button from "../UI/Button"
 import { useTranslation } from 'react-i18next';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
+import { MyContext } from '../../context/MyContext';
 const Home = () => {
     const { t } = useTranslation();
+    const { userContext,setUserId,setUserName } = useContext(MyContext);
+    let backEndUrl = userContext.backEndUrl
+    const [userLoggedIn, setUserLoggedIn] = useState(false)
     const [showModal, setShowModal] = useState(true);
     const handleStartAdventure = () => {
         setShowModal(false);
@@ -15,16 +21,48 @@ const Home = () => {
     const changeActivePage = (page) => {
         setActivePage(page)
     }
+    const handleGoogleLogin = (credentials) => {
+        const parameters = [{
+            credentials: credentials
+        }]
+        fetch(backEndUrl + "/login", {
+            method: 'POST', // O 'PUT' si deseas sobrescribir completamente los datos del usuario
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(parameters),
+        }).then(response => response.json())
+        .then((data) => {
+            if (data.user_id){
+                setUserId(data.user_id)
+                setUserName(data.name)
+                setUserLoggedIn(true)
+            }
+        })
+    }
     return (
-        <div id="homeDiv" className={classes.homeDiv}>
-            {showModal && <Modal onClose={handleStartAdventure} color="white"  >
-                <h1 >{t('home.welcome')}</h1>
-                <p style={{ color: '#fff' }}>{t('home.getReady')}</p>
-                <Button colorType="green" value={t('home.start')} onClick={() => handleStartAdventure()}>
-                </Button></Modal>}
-            {activePage && activePage === 1 && !showModal && <Multiverse changeActivePage={changeActivePage}></Multiverse>}
-            {activePage && activePage === 2 && !showModal && <Battleground changeActivePage={changeActivePage}></Battleground>}
-        </div>
+        <GoogleOAuthProvider clientId="297991534299-1ed49hpjqhhudbcngaa0an7b0jts398v.apps.googleusercontent.com">
+            {!userLoggedIn ?
+                <div id="homeDivGoogle" className={classes.homeDivGoogle}><GoogleLogin
+                    onSuccess={credentialResponse => {
+                        handleGoogleLogin(credentialResponse)
+                    }}
+                    onError={() => {
+                        console.log('Login Failed');
+                    }}
+                    useOneTap
+                /> </div> :
+                <div id="homeDiv" className={classes.homeDiv}>
+                    {showModal && <Modal onClose={handleStartAdventure} color="white"  >
+                        <h1 >{t('home.welcome')}</h1>
+                        <p style={{ color: '#fff' }}>{t('home.getReady')}</p>
+                        <Button colorType="green" value={t('home.start')} onClick={() => handleStartAdventure()}>
+                        </Button></Modal>}
+                    {activePage && activePage === 1 && !showModal && <Multiverse changeActivePage={changeActivePage}></Multiverse>}
+                    {activePage && activePage === 2 && !showModal && <Battleground changeActivePage={changeActivePage}></Battleground>}
+                </div>
+            }
+        </GoogleOAuthProvider>
     )
 }
 
