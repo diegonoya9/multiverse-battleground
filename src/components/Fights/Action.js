8643e1f1465e89cf997b138af148c555b028c525
Diego Noya <diegonoya9@gmail.com>
2024-01-16 14:28:56 -0300

    fixed actions icons

--- src/components/Fights/Action.js
+++ src/components/Fights/Action.js
@@ -2,10 +2,15 @@ import classes from './Action.module.css'
 const Action = ({ action }) => {
     const actionIdentifier = {
         "current_hp": "/assets/img/HP.png",
+        "extra_defense": '/assets/img/defense.png',
         "defense": '/assets/img/defense.png',
+        "extra_special_defense": '/assets/img/specialDefense.png',
         "special_defense": '/assets/img/specialDefense.png',
+        "extra_attack": '/assets/img/attack.png',
         "attack": '/assets/img/attack.png',
+        "extra_special_attack": '/assets/img/specialAttack.png',
         "special_attack": '/assets/img/specialAttack.png',
+        "extra_accuracy": "/assets/img/accuracy.png",
         "accuracy": "/assets/img/accuracy.png"
     }
     return (
