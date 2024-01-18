f71941d6ffa800d4211cac534ba9b933c3208b71
Diego Noya <diegonoya9@gmail.com>
2024-01-18 09:24:49 -0300

    added more i18n to objectsCard

--- src/components/UI/ObjectCard.js
+++ src/components/UI/ObjectCard.js
@@ -7,11 +7,11 @@ const ObjectCard = ({ object, showPrice }) => {
             <div className={` ${classes.face} ${classes.front}`}>
                 {showPrice && <p className={classes.spanStats}>{t('objectscard.price')}:{object.price}</p>}
                 <img alt="object" src={object.img} className={classes.objectImg} />
-                <h3 className={`${classes.objectName} ${classes.title} `}>{object.name}</h3>
+                <h3 className={`${classes.objectName} ${classes.title} `}>{t(`objects.${object.name}`)}</h3>
                 <h2 className={classes.spanStats}>{t('objectscard.quantity')}:{object.quantity}</h2>
             </div>
             <div className={`${classes.divStats} ${classes.face} ${classes.back}  ${object.active === "true" && classes.active}`} key={`${object.id}stats`}>
-                <span className={classes.spanStats}>{t('objectscard.description')}:{object.description}</span>
+                <span className={classes.spanStats}>{t('objectscard.description')}:{t(`objects.${object.name}_d`)}</span>
             </div>
         </div >
     )
