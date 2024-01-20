8dc7c08d5f89db0a9f2013058ce1d40c78b6b351
Diego Noya <diegonoya9@gmail.com>
2024-01-20 10:54:01 -0300

    fixed scroll when the modal is open

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,17 +1,34 @@
-// Modal.js
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import classes from './Modal.module.css';
 
 const Modal = ({ onClose, backgroundColor, color, children, styleType }) => {
     const modalRef = useRef();
+    const originalScrollPosition = useRef(0);
+    // Bloquear el scroll cuando se monta el modal
+    useEffect(() => {
+        let root = document.getElementById("root")
+        const originalOverflow = root.style.overflow;
+        const originalHeight = root.style.height;
+
+        // Bloquear el scroll al montar el modal
+        root.style.overflow = 'hidden';
+        root.style.height = '100vh';
+
+        // Desbloquear el scroll al desmontar el modal
+        return () => {
+            root.style.overflow = originalOverflow;
+            root.style.height = originalHeight;
+        };
+    }, []);
 
     // Agregar un event listener para cerrar el modal haciendo clic fuera de él
-    React.useEffect(() => {
+    useEffect(() => {
         const handleClickOutside = (event) => {
             if (modalRef.current && !modalRef.current.contains(event.target)) {
                 onClose();
             }
         };
+
         document.addEventListener('mousedown', handleClickOutside);
 
         return () => {
@@ -21,7 +38,7 @@ const Modal = ({ onClose, backgroundColor, color, children, styleType }) => {
 
     return (
         <div className={classes.modalWrapper} style={{ backgroundColor, color }}>
-            <div className={classes.modalContainer} style={{ color }} >
+            <div className={classes.modalContainer} style={{ color }}>
                 <div className={`${classes.modalContent} ${classes[styleType]}`} style={{ color }} ref={modalRef}>
                     {children}
                 </div>
@@ -29,6 +46,5 @@ const Modal = ({ onClose, backgroundColor, color, children, styleType }) => {
         </div>
     );
 };
-
 
 export default Modal;
